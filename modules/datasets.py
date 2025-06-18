f0d50912dfc125468ddc5f3a6a4ea74412c10130
CQNU-ZhangLab <163380281+CQNU-ZhangLab@users.noreply.github.com>
2025-06-18 19:01:57 +0800

    add the required module files

---
+++ modules/datasets.py
@@ -0,0 +1,93 @@
+import json
+import os
+
+import cv2
+import torch
+from PIL import Image, ImageFilter
+from torch.utils.data import Dataset
+from wandb.util import np
+
+
+class BaseDataset(Dataset):
+    def __init__(self, args, tokenizer, split, transform=None):
+        self.image_dir = args.image_dir
+        self.image_seg_dir = args.image_seg_dir
+        self.ann_path = args.ann_path
+        self.max_seq_length = args.max_seq_length
+        self.split = split
+        self.tokenizer = tokenizer
+        self.transform = transform
+        self.ann = json.loads(open(self.ann_path, 'r').read())
+        self.examples = self.ann[self.split]
+        for i in range(len(self.examples)):
+            self.examples[i]['ids'] = tokenizer(self.examples[i]['report'])[:self.max_seq_length]
+            self.examples[i]['mask'] = [1] * len(self.examples[i]['ids'])
+
+    def __len__(self):
+        return len(self.examples)
+
+
+class IuxrayMultiImageDataset(BaseDataset):
+    def __getitem__(self, idx):
+        example = self.examples[idx]
+        image_id = example['id']
+        image_path = example['image_path']
+        image_path_1 = os.path.join(self.image_dir, image_path[0])
+        image_1 = enhance_image(image_path_1)
+        image_path_2 = os.path.join(self.image_dir, image_path[0])
+        image_2 = enhance_image(image_path_2)
+        if self.transform is not None:
+            image_1 = self.transform(image_1)
+            image_2 = self.transform(image_2)
+        image = torch.stack((image_1, image_2), 0)
+        report_ids = example['ids']
+        report_masks = example['mask']
+        seq_length = len(report_ids)
+        sample = (image_id, image, report_ids, report_masks, seq_length)
+        return sample
+
+
+def enhance_image(image_path):
+    image = Image.open(image_path).convert('RGB')
+    image = np.array(image)
+    image_yuv = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+    image_yuv[:, :, 0] = cv2.equalizeHist(image_yuv[:, :, 0])
+    image = cv2.cvtColor(image_yuv, cv2.COLOR_YUV2RGB)
+    image_pil = Image.fromarray(image)
+    image_pil = image_pil.filter(ImageFilter.SHARPEN)
+    return image_pil
+
+
+class MimiccxrSingleImageDataset(BaseDataset):
+    def __getitem__(self, idx):
+        example = self.examples[idx]
+        image_id = example['id']
+        image_path = example['image_path']
+        image = Image.open(os.path.join(self.image_dir, image_path[0])).convert('RGB')
+        image_id = os.path.join(self.image_dir, image_path[0])
+        if self.transform is not None:
+            image = self.transform(image)
+        report_ids = example['ids']
+        report_masks = example['mask']
+        seq_length = len(report_ids)
+        sample = (image_id, image, report_ids, report_masks, seq_length)
+        return sample
+
+
+class MimiccxrDualImageDataset(BaseDataset):
+    def __getitem__(self, idx):
+        example = self.examples[idx]
+        image_id = example['id']
+        image_path = example['image_path']
+        image_path_1 = os.path.join(self.image_dir, image_path[0])
+        image_1 = enhance_image(image_path_1)
+        image_2 = Image.open(os.path.join(self.image_seg_dir, image_path[0])).convert('RGB')
+        if self.transform is not None:
+            image_1 = self.transform(image_1)
+            image_2 = self.transform(image_2)
+        image = torch.stack((image_1, image_2), 0)
+        report_ids = example['ids']
+        report_masks = example['mask']
+        seq_length = len(report_ids)
+        sample = (image_id, image, report_ids, report_masks, seq_length)
+        return sample
